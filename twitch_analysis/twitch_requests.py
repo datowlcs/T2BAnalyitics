b80e76f1e86c1a7fe8097b63b71d836128003476
Tanner <tdyer@hawk.iit.edu>
2020-10-18 01:55:14 -0500

    first attempt at writeToDB function for live stream data

--- twitch_analysis/twitch_requests.py
+++ twitch_analysis/twitch_requests.py
@@ -1,12 +1,23 @@
 import json
 import requests
+import psycopg2
+import time
 
+
+# API variables
 BASE_URL = 'https://api.twitch.tv/helix/'
 CLIENT_ID = 'ns8jo098eiow520zuf3ltf7x14erc0'
 CLIENT_SECRET = '68wkijs855octdmp6mfzqk8ygztiqz'
-#HEADERS = {'client-id': CLIENT_ID, 'Authorization': 'Bearer ' + ACCESS_TOKEN}
 INDENT = 2
 AUTH_URL = 'https://id.twitch.tv/oauth2/token'
+
+
+# Workflow for fetching data from Twitch API
+# 1. Get auth token
+# 2. Generate query url
+# 3. Call Twitch API
+# 4. Handle Response
+# 5. Save to Database
 
 
 def get_twitch_auth():
@@ -49,15 +60,73 @@ def get_games_query():
 def print_response(response):
     print(response.json()["data"][0]["viewer_count"])
 
+
+def writeLiveStreamDataToDB(user_login, game_id, viewer_count, started_at, language):
+    # DB variables
+    host = 't2banalyticsserver.postgres.database.azure.com'
+    dbname = 'streamerliveanalytics'
+    user = 'TannerTTV@t2banalyticsserver'
+    password = 'AlexLinaNolan!'
+    sslmode = 'require'
+    table_name = user_login
+
+    # connect to db
+    conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+    conn = psycopg2.connect(conn_string)
+
+    cursor = conn.cursor()
+
+    # check if streamer table already exists
+    cursor.execute("select exists(select * from information_schema.tables where table_name=%s)", (table_name,))
+
+    # create table for streamer if none exist
+    if not bool(cursor.rowcount):
+        create_table = """
+        CREATE TABLE {} (
+        timestamp INTEGER PRIMARY KEY,
+        game_id INTEGER NOT NULL,
+        viewer_count INTEGER,
+        started_at INTEGER NOT NULL,
+        language VARCHAR(32) NOT NULL
+        );""".format(table_name)
+
+        cursor.execute(create_table)
+    
+    # write live stream data to table
+    insert_data = """
+    INSERT INTO {} (timestamp, game_id, viewer_count, started_at, language) VALUES
+    ({}, {}, {}, {}, {});""".format(time.time, game_id, viewer_count, started_at, language)
+
+    cursor.execute(insert_data)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+
 if __name__ == "__main__":
-    get_twitch_auth()
+    
+    # users to fetch live stream data from
+    user_login = 'Sykkuno'
 
-    user_login = 'sodapoppin'
-
+    # obtain auth token
     access_token = get_twitch_auth()
 
+    # fetch stream data from api
     query = get_user_streams_query(user_login)
-    #query = twitch_requests.get_games_query()
     response = get_response(query, access_token)
 
-    print_response(response)
+    #print_response(response)
+
+    # TODO: Parse response to obtain stream data values
+
+    # stream data
+    game_id = 0
+    viewer_count = 0
+    started_at = 0
+    language = ""
+
+    writeLiveStreamDataToDB(user_login, game_id, viewer_count, started_at, language)
+
+    
